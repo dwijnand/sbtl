938c3207b0943ce53e58e6268c9c3e97fd000f89
Dale Wijnand <dale.wijnand@gmail.com>
2016-12-17 13:05:40 +0000

    Handle failure to exec command

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::process::Command;
+use std::process::exit;
 use std::os::unix::process::CommandExt;
 
 fn main() {
-    Command::new("java")
+    let err = Command::new("java")
         .arg("-Xms512m")
         .arg("-Xmx1536m")
         .arg("-Xss2m")
         .args(&["-jar", "/Users/dnw/.sbt/launchers/0.13.13/sbt-launch.jar"])
         .arg("shell")
         .exec();
+    println!("error: {}", err);
+    if let Some(err) = err.raw_os_error() {
+        exit(err);
+    }
+    exit(-1)
 }
