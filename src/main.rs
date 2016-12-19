0e5a2a44fc9ca89680d1b70e45b74ae1721aa003
Dale Wijnand <dale.wijnand@gmail.com>
2016-12-19 17:30:32 +0000

    Make launcher path dependent on HOME

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use std::process::exit;
 use std::os::unix::process::CommandExt;
 
 fn main() {
+    let home = std::env::var("HOME").unwrap();
     let err = Command::new("java")
         .arg("-Xms512m")
         .arg("-Xmx1536m")
         .arg("-Xss2m")
-        .args(&["-jar", "/Users/dnw/.sbt/launchers/0.13.13/sbt-launch.jar"])
+        .args(&["-jar", &format!("{}/.sbt/launchers/0.13.13/sbt-launch.jar", home)])
         .arg("shell")
         .exec();
     println!("error: {}", err);
