9597900c5d14d078474c2a2b844edbc2d3ca3791
Dale Wijnand <dale.wijnand@gmail.com>
2016-12-22 11:13:31 +0000

    Condense

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
-use std::process::Command;
-use std::process::exit;
-use std::os::unix::process::CommandExt;
-
 fn main() {
     let home = std::env::var("HOME").unwrap();
-    let err = Command::new("java")
-        .arg("-Xms512m")
-        .arg("-Xmx1536m")
-        .arg("-Xss2m")
+    use std::os::unix::process::CommandExt;
+    let err = std::process::Command::new("java")
+        .args(&["-Xms512m", "-Xmx1536m", "-Xss2m"])
         .args(&["-jar", &format!("{}/.sbt/launchers/0.13.13/sbt-launch.jar", home)])
         .arg("shell")
         .exec();
     println!("error: {}", err);
     if let Some(err) = err.raw_os_error() {
-        exit(err);
+        std::process::exit(err);
     }
-    exit(-1)
+    std::process::exit(-1)
 }
