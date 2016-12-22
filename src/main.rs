53d5c49c79e1da1d71cee7a20be3ff9272fe0a70
Dale Wijnand <dale.wijnand@gmail.com>
2016-12-22 11:25:13 +0000

    Favour Path over String for paths

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
-    let home = std::env::var("HOME").unwrap();
+    let home = std::env::home_dir().unwrap();
+    let sbt_jar;
+    {
+        let mut p = std::path::PathBuf::from(home);
+        p.push(".sbt/launchers/0.13.13/sbt-launch.jar");
+        sbt_jar = p;
+    }
     use std::os::unix::process::CommandExt;
     let err = std::process::Command::new("java")
         .args(&["-Xms512m", "-Xmx1536m", "-Xss2m"])
-        .args(&["-jar", &format!("{}/.sbt/launchers/0.13.13/sbt-launch.jar", home)])
+        .args(&[&"-jar".as_ref(), &sbt_jar.as_os_str()])
         .arg("shell")
         .exec();
     println!("error: {}", err);
