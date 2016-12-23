24e3526ba1412f6fa2bf7e4e51bb27784db3cb08
Dale Wijnand <dale.wijnand@gmail.com>
2016-12-23 16:23:14 +0100

    Use shadowing over blocks

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 fn main() {
     let home = std::env::home_dir().unwrap();
-    let sbt_jar;
-    {
-        let mut p = std::path::PathBuf::from(home);
-        p.push(".sbt/launchers/0.13.13/sbt-launch.jar");
-        sbt_jar = p;
-    }
+    let mut sbt_jar = std::path::PathBuf::from(home);
+    sbt_jar.push(".sbt/launchers/0.13.13/sbt-launch.jar");
+    let sbt_jar = sbt_jar;
     use std::os::unix::process::CommandExt;
     let err = std::process::Command::new("java")
         .args(&["-Xms512m", "-Xmx1536m", "-Xss2m"])
