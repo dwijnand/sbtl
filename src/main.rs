c3a52b2674adaaf0049fba31c8f09f2d215bb355
Dale Wijnand <dale.wijnand@gmail.com>
2017-07-28 08:15:58 +0100

    sbt 0.13.16

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use std::os::unix::process::CommandExt;
 use std::path::{ Path, PathBuf, };
 use std::process::{ Command, exit, };
 
-const sbt_release_version: &'static str = "0.13.14";
+const sbt_release_version: &'static str = "0.13.16";
 
 const buildProps: &'static str = "project/build.properties";
 
